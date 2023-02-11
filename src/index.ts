517d2daf6a0f18ae0be6bf50b5878bd398dc9e50
Chris NeJame <chris.t.nejame@gmail.com>
2023-02-11 15:14:40 -0500

    fix: should export number error too

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {
   ObjectAssertionError,
   ArrayAssertionError,
   StringAssertionError,
+  NumberAssertionError,
 } from "./Errors";
 export { assertIsObject, isObject } from "./Object";
 export { assertIsString, isString } from "./String";
