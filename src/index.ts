762947551c7b010b08d5ae83777b460d866ef951
Chris NeJame <chris.t.nejame@gmail.com>
2024-03-31 11:43:07 -0400

    fix: added ArrayOf functions to index so they can be imported properly

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { assertIsArray, isArray } from "./Array.js";
+export { assertIsArrayOf, isArrayOf } from "./ArrayOf.js";
 export { assertIsBigInt, isBigInt } from "./BigInt.js";
 export { assertIsBoolean, isBoolean } from "./Boolean.js";
 export {
